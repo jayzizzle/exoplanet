33a65b6c4a1702b9323be3a6419f8a261e93fcdc
jayzizzle <dj.jonzamora@gmail.com>
2022-02-23 11:07:33 -0800

    clear comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,6 @@ import { SelectAxes } from './SelectAxes';
 
 import { Test } from './Test';
 
-// HAPPY HOLIDAYS AND THANK YOU FOR THIS OPPORTUNITY!
-// This is my first experience with D3 so I spent a majority of time doing some research on the library.
-// Here are a couple notes:
-// I 'hard coded' only the CSV columns that contained positive numeric values (also non-exponent).
-// I was unable to finish adding the Histogram.
-// If given more time, I would also go back and refactor a few things to be more DRY,
-// Do more styling,
-// And optimize the D3 portions better.
-
 export const Home = () => {
   const [results, setResults] = useState();
 
